5fcc129526801ba33c709f4909fefa67e3186d6e
Juan Campo <camilo.013campo@gmail.com>
2025-05-15 12:57:52 -0500

    Inicializar proyecto impulsaColBackend

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (_req, res) => {
+    res.send('¡ImpulsoCol Backend está funcionando!');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
